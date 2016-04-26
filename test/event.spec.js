d5d2dae6e093a2644b6d31225172f733678edc45
eripheebs <erikapheby@gmail.com>
2016-04-26 19:54:19 +0100

    second chai test working

--- test/event.spec.js
+++ test/event.spec.js
@@ -12,6 +12,26 @@ describe('events', function(){
       .get('/events')
       .end(function(err, res){
         expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        done();
+      });
+  });
+
+  it('should add an event on /events POST', function(done){
+    chai.request(server)
+      .post('/events')
+      .send({'title': 'fakeTitle', 'description': 'fakeDescription', 'time': '24/11/1993', 'location': 'kylesBed'})
+      .end(function(err, res){
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body[0]).to.be.a('object');
+        expect(res.body[0]).to.have.property('title');
+        expect(res.body[0]).to.have.property('description');
+        expect(res.body[0]).to.have.property('id');
+        expect(res.body[0].title).to.equal('fakeTitle');
+        expect(res.body[0].description).to.equal('fakeDescription');
         done();
       });
   });
