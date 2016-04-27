efd0f7f56d1007831c5310a994d39ecdf19c539a
Kyle Buttner <kylebuttner@gmail.com>
2016-04-27 13:58:26 +0100

    create tests for events crud functions

--- routes/events.js
+++ routes/events.js
@@ -8,103 +8,59 @@ var models  = require('../models');
 router.get('/', function(req, res) {
   models.Event.findAll({}).then(function(dump){
     res.json(dump);
-  })
+  });
 });
 
 router.post('/', function(req, res) {
+  models.Event.create({
+    title: req.body.title,
+    description: req.body.description,
+    time: req.body.time,
+    location: req.body.location
+  }).then(function(Event) {
+    res.json(Event);
+  });
+});
 
-    var results = [];
-    var data = {title: req.body.title,
-      description: req.body.description,
-      time: req.body.time,
-      location: req.body.location
-    };
-
-    pg.connect(connectionString, function(err, client, done) {
-
-        client.query("INSERT INTO events(title, description, time, location) values($1, $2, $3, $4)",
-        [data.title, data.description, data.time, data.location]);
-
-        var query = client.query("SELECT * FROM events ORDER BY id ASC");
-
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        query.on('end', function() {
-            client.end();
-            return res.json(results);
-        });
-
-        if(err) {
-          console.log(err);
-        }
-
-    });
+router.get('/:event_id', function(req, res) {
+  models.Event.find({
+    where: {
+      id: req.params.event_id
+    }
+  }).then(function(Event){
+    res.json(Event)
+  });
 });
 
 router.put('/:event_id', function(req, res) {
-
-    var results = [];
-
-    var id = req.params.event_id;
-
-    var data = {title: req.body.title,
-      description: req.body.description,
-      time: req.body.time,
-      location: req.body.location
-    };
-
-    pg.connect(connectionString, function(err, client, done) {
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).send(json({ success: false, data: err}));
-        }
-
-        client.query("UPDATE events SET title=($1), description=($2), time=($3), location=($4) WHERE id=($5)", [data.title, data.description, data.time, data.location, id]);
-
-        var query = client.query("SELECT * FROM events ORDER BY id ASC");
-
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
-    });
-
+  models.Event.find({
+    where: {
+      id: req.params.id
+    }
+  }).then(function(Event){
+    if(Event){
+      Event.updateAttributes({
+        title: req.body.title,
+        description: req.body.description,
+        time: req.body.description,
+        location: req.body.description
+      }).then(function(Event) {
+        res.send(Event);
+      });
+    }
+  }).then(function(Event){
+    res.json(Event)
+  });
 });
 
 router.delete('/:event_id', function(req, res) {
-
-    var results = [];
-
-    var id = req.params.event_id;
-
-
-    pg.connect(connectionString, function(err, client, done) {
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).json({ success: false, data: err});
-        }
-
-        client.query("DELETE FROM events WHERE id=($1)", [id]);
-
-        var query = client.query("SELECT * FROM events ORDER BY id ASC");
-
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
-    });
+  models.Event.destroy({
+    where: {
+      id: req.params.event_id
+    }
+  }).then(function(Event) {
+    res.json(Event);
+  })
 
 });
 
--- test/event.spec.js
+++ test/event.spec.js
@@ -15,6 +15,7 @@ describe('events', function(){
     chai.request(server)
       .get('/events')
       .end(function(err, res){
+        console.log(res.body)
         expect(res).to.have.status(200);
         expect(res).to.be.json;
         expect(res.body).to.be.a('array');
@@ -29,33 +30,48 @@ describe('events', function(){
       .end(function(err, res){
         expect(res).to.have.status(200);
         expect(res).to.be.json;
-        expect(res.body).to.be.a('array');
-        expect(res.body[0]).to.be.a('object');
-        expect(res.body[0]).to.have.property('title');
-        expect(res.body[0]).to.have.property('description');
-        expect(res.body[0]).to.have.property('id');
-        expect(res.body[0].title).to.equal('fakeTitle');
-        expect(res.body[0].description).to.equal('fakeDescription');
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('title');
+        expect(res.body).to.have.property('description');
+        expect(res.body).to.have.property('id');
+        expect(res.body.title).to.equal('fakeTitle');
+        expect(res.body.description).to.equal('fakeDescription');
+        done();
+      });
+  });
+
+  it('should get a SINGLE event on /event/<id> GET', function(done) {
+    chai.request(server)
+      .get('/events/4')
+      .end(function(err, res){
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('title');
+        expect(res.body).to.have.property('description');
+        expect(res.body).to.have.property('id');
+        expect(res.body.title).to.equal('fakeTitle');
+        expect(res.body.description).to.equal('fakeDescription');
         done();
       });
   });
 
   it('should update a SINGLE event on /event/<id> PUT', function(done) {
     chai.request(server)
-      .get('/events')
+      .put('/events/1')
+      .send({'title': 'newTitle', 'description': 'newDescription', 'time': '24/11/1993', 'location': 'kylesBed'})
       .end(function(err, res){
-        chai.request(server)
-          .put('/events/'+res.body[1].id)
-          .send({'title': 'changeTitle', 'description': 'fakeDescription', 'time': '24/11/1993', 'location': 'kylesBed'})
-          .end(function(error, response){
-            expect(response).to.have.status(200);
-            expect(response).to.be.json;
-            expect(response.body).to.be.a('array');
-            expect(response.body[1]).to.be.a('object');
-            expect(response.body[1]).to.have.property('title');
-            expect(response.body[1].title).to.equal('changeTitle');
-            done();
-        });
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should delete a SINGLE event on /event/<id> DELETE', function(done) {
+    chai.request(server)
+      .delete('/events/1')
+      .end(function(err, res){
+        expect(res).to.have.status(200);
+        done();
       });
   });
 });
