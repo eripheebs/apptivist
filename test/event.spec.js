4f1fa3f4154aa082ae1df89fda9b53d659ad463b
eripheebs <erikapheby@gmail.com>
2016-04-27 17:50:54 +0100

    updated tests for route changes

--- test/event.spec.js
+++ test/event.spec.js
@@ -13,7 +13,7 @@ describe('events', function(){
 
   it('should list all events on /events GET', function(done){
     chai.request(server)
-      .get('/events')
+      .get('/api/events')
       .end(function(err, res){
         console.log(res.body)
         expect(res).to.have.status(200);
@@ -25,7 +25,7 @@ describe('events', function(){
 
   it('should add an event on /events POST', function(done){
     chai.request(server)
-      .post('/events')
+      .post('/api/events')
       .send({'title': 'fakeTitle', 'description': 'fakeDescription', 'time': '24/11/1993', 'location': 'kylesBed'})
       .end(function(err, res){
         expect(res).to.have.status(200);
@@ -42,7 +42,7 @@ describe('events', function(){
 
   it('should get a SINGLE event on /event/<id> GET', function(done) {
     chai.request(server)
-      .get('/events/4')
+      .get('/api/events/4')
       .end(function(err, res){
         expect(res).to.have.status(200);
         expect(res).to.be.json;
@@ -58,7 +58,7 @@ describe('events', function(){
 
   it('should update a SINGLE event on /event/<id> PUT', function(done) {
     chai.request(server)
-      .put('/events/1')
+      .put('/api/events/1')
       .send({'title': 'newTitle', 'description': 'newDescription', 'time': '24/11/1993', 'location': 'kylesBed'})
       .end(function(err, res){
         expect(res).to.have.status(200);
@@ -68,7 +68,7 @@ describe('events', function(){
 
   it('should delete a SINGLE event on /event/<id> DELETE', function(done) {
     chai.request(server)
-      .delete('/events/1')
+      .delete('/api/events/1')
       .end(function(err, res){
         expect(res).to.have.status(200);
         done();
