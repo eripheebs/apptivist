dd1c87d9a47fdcbf6fb3b3d51f61b136d6cf0e5c
eripheebs <erikapheby@gmail.com>
2016-04-27 16:20:33 +0100

    fixed pathways that werent right after merging frontend and backend

--- test/event.spec.js
+++ test/event.spec.js
@@ -4,7 +4,7 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var shouldHttp = require('should-http');
 var server = require('../app');
-var Event = require('../routes/events');
+var Event = require('../server/routes/events');
 var expect = chai.expect;
 
 chai.use(chaiHttp);
@@ -46,7 +46,7 @@ describe('events', function(){
       .end(function(err, res){
         expect(res).to.have.status(200);
         expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
+        expect(res).to.be.a('object');
         expect(res.body).to.have.property('title');
         expect(res.body).to.have.property('description');
         expect(res.body).to.have.property('id');
