7b51ab750c5bb8521d393dad0aa2b8e651f16c17
Kyle Buttner <kylebuttner@gmail.com>
2016-04-27 14:59:51 +0100

    added client and server files

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.postgreSQL = {
-  development: 'postgres://localhost/apptivist_development',
-  test: 'postgres://localhost/apptivist_test'
-};
-
-module.exports = config;
