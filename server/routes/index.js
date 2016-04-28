9fe69d567c89f40b4d2008393cbbf79f99fa2295
Robota <lorenzo.turrino@gmail.com>
2016-04-28 12:29:37 +0100

    passport tests

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var app = express();
 
 router.get('/', function (req, res) {
+  console.log('MAKING AN EVENT PAGELOAD REQUEST WITH:',req.user);
   res.render('index');
 });
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var passport = require('passport');
+var GithubStrategy = require('passport-github').Strategy;
+var session = require('express-session');
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/auth', passport.authenticate('github'));
+
+router.get('/auth/callback', passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    // console.log("success for", req.user.username);
+    res.redirect('/');
+  }
+);
+
+router.get('/logout', function(req, res){
+  console.log('logging out');
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
